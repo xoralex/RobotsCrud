84b2dcf605c8af539dfed784a5fdef4388751361
Alexandr Dorofeyev <xoralex@gmail.com>
2015-07-22 10:30:31 +0600

    Переименовал интерфейс и его класс согласно Java Naming Conventions

---
+++ app/src/main/java/com/dorofeyev/robotscrud/RestRobotDAO.java
@@ -0,0 +1,88 @@
+package com.dorofeyev.robotscrud;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by xor on 7/20/15.
+ */
+public class RobotDAO implements IRobotDAO {
+    @Override
+    public void create(Robot robot)  {
+
+    }
+
+    @Override
+    public List<Robot> read(final Callback callback)  {
+
+
+        RobotRestClient.get("", null, new JsonHttpResponseHandler() {
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                AsyncHttpClient.log.w("JsonHttpRH", "onSuccess(int, Header[], JSONObject) was not overriden, but callback was received");
+            }
+
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                ObjectMapper mapper = new ObjectMapper();
+
+                try {
+                    List<Robot> robots = null;
+                    robots = mapper.readValue(response.toString(), List.class);
+                    callback.execute(robots);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+
+            }
+
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+
+            }
+
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+
+            }
+
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+
+            }
+
+            int x = 5+5;
+
+        });
+
+
+
+
+
+
+        return null;
+    }
+
+    @Override
+    public void update(Robot robot) {
+
+    }
+
+    @Override
+    public void delete(Robot robot) {
+
+    }
+
+    @Override
+    public Robot fetchById(int id) {
+        return null;
+    }
+}
--- app/src/main/java/com/dorofeyev/robotscrud/RobotDAO.java
+++ app/src/main/java/com/dorofeyev/robotscrud/RobotDAO.java
@@ -1,88 +1,20 @@
 package com.dorofeyev.robotscrud;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.JsonHttpResponseHandler;
-
-import org.apache.http.Header;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-
-import java.io.IOException;
 import java.util.List;
 
 /**
  * Created by xor on 7/20/15.
  */
-public class RobotDAO implements IRobotDAO {
-    @Override
-    public void create(Robot robot)  {
+public interface IRobotDAO {
 
-    }
+    public void create(Robot robot);
 
-    @Override
-    public List<Robot> read(final Callback callback)  {
+    public List<Robot> read(Callback command);
 
+    public void update(Robot robot);
 
-        RobotRestClient.get("", null, new JsonHttpResponseHandler() {
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                AsyncHttpClient.log.w("JsonHttpRH", "onSuccess(int, Header[], JSONObject) was not overriden, but callback was received");
-            }
+    public void delete(Robot robot);
 
-            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                ObjectMapper mapper = new ObjectMapper();
+    public Robot fetchById(int id);
 
-                try {
-                    List<Robot> robots = null;
-                    robots = mapper.readValue(response.toString(), List.class);
-                    callback.execute(robots);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-
-            }
-
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
-
-            }
-
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-
-            }
-
-            public void onSuccess(int statusCode, Header[] headers, String responseString) {
-
-            }
-
-            int x = 5+5;
-
-        });
-
-
-
-
-
-
-        return null;
-    }
-
-    @Override
-    public void update(Robot robot) {
-
-    }
-
-    @Override
-    public void delete(Robot robot) {
-
-    }
-
-    @Override
-    public Robot fetchById(int id) {
-        return null;
-    }
 }
