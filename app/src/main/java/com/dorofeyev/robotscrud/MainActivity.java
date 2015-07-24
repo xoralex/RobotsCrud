8c6cc4cb2f39daef5b20418cd93c71b0cd57815a
Alexandr Dorofeyev <xoralex@gmail.com>
2015-07-24 14:19:07 +0600

    Сделал диалоговое окно добавления робота болле универсальным, чтобы его можно было использовать и для редактирования робота.

--- app/src/main/java/com/dorofeyev/robotscrud/MainActivity.java
+++ app/src/main/java/com/dorofeyev/robotscrud/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -87,7 +86,8 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void menuAddClicked() {
-        AddRobotDialogFragment addRobotDialogFragment = new AddRobotDialogFragment();
+        RobotDialogFragment addRobotDialogFragment = new RobotDialogFragment();
+        addRobotDialogFragment.setTitle((String) getText(R.string.add_robot_dialog_header));
         addRobotDialogFragment.show(getFragmentManager(), "");
     }
 
--- app/src/main/java/com/dorofeyev/robotscrud/RobotDialogFragment.java
+++ app/src/main/java/com/dorofeyev/robotscrud/RobotDialogFragment.java
@@ -10,14 +10,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class AddRobotDialogFragment extends DialogFragment {
+public class RobotDialogFragment extends DialogFragment {
+
+    private String title;
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_dialog_add_robot, container,
+        View rootView = inflater.inflate(R.layout.fragment_dialog_robot, container,
                 false);
-        getDialog().setTitle(R.string.add_robot_dialog_header);
+        getDialog().setTitle(title);
 
         Button addRobotButton = (Button) rootView.findViewById(R.id.buttonAddRobot);
         addRobotButton.setOnClickListener(new View.OnClickListener() {
