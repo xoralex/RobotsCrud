2f030d79ac8bc1bb62d13e8c33271b548d917f0b
Alexandr Dorofeyev <xoralex@gmail.com>
2015-07-26 21:09:00 +0600

    Убрал ненужную опцию Settings из главного меню

--- app/src/main/java/com/dorofeyev/robotscrud/MainActivity.java
+++ app/src/main/java/com/dorofeyev/robotscrud/MainActivity.java
@@ -66,11 +66,7 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        else if (id == R.id.action_add) {
+        if (id == R.id.action_add) {
             menuAddClicked();
         }
         else if (id == R.id.action_refresh) {
